04a0df817a1f6b2d50e9b7ec0b08be36406d846f
Gennady Verdel <godrose@gmail.com>
2017-07-18 16:37:14 +0300

    Moved portable project to the correspondent folder

---
+++ src/LogoFX.Client.Testing.Integration.SpecFlow/src/BuilderRegistrationService.cs
@@ -0,0 +1,19 @@
+﻿using Attest.Fake.Builders;
+using Attest.Testing.SpecFlow;
+using LogoFX.Client.Testing.Contracts;
+
+namespace LogoFX.Client.Testing.Integration.SpecFlow
+{
+    /// <summary>
+    /// Represents builder registration service for SpecFlow-based integration tests.
+    /// </summary>
+    /// <seealso cref="StepsBase" />
+    /// <seealso cref="IBuilderRegistrationService" />
+    public class BuilderRegistrationService : StepsBase, IBuilderRegistrationService
+    {
+        void IBuilderRegistrationService.RegisterBuilder<TService>(FakeBuilderBase<TService> builder)
+        {
+            RegisterBuilder(builder);
+        }
+    }   
+}
