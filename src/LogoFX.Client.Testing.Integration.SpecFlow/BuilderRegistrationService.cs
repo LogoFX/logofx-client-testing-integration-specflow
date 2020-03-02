0b450b0bdf4a60091c56bad70a9b5c7927a299d4
Gennady Verdel <godrose@gmail.com>
2020-03-02 10:55:19 +0200

    feat(parallelism): injecting scenario context per test closes #8

--- src/LogoFX.Client.Testing.Integration.SpecFlow/BuilderRegistrationService.cs
+++ src/LogoFX.Client.Testing.Integration.SpecFlow/BuilderRegistrationService.cs
@@ -1,19 +1,30 @@
-﻿using Attest.Testing.Contracts;
-using Attest.Testing.SpecFlow;
+﻿using Attest.Fake.Registration;
+using Attest.Testing.Contracts;
 using Solid.Patterns.Builder;
+using Solid.Practices.IoC;
 
 namespace LogoFX.Client.Testing.Integration.SpecFlow
 {
     /// <summary>
     /// Represents builder registration service for SpecFlow-based integration tests.
     /// </summary>
-    /// <seealso cref="StepsBase" />
     /// <seealso cref="IBuilderRegistrationService" />
-    public class BuilderRegistrationService : StepsBase, IBuilderRegistrationService
+    public class BuilderRegistrationService : IBuilderRegistrationService
     {
+        private readonly IDependencyRegistrator _dependencyRegistrator;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BuilderRegistrationService"/> class.
+        /// </summary>
+        /// <param name="dependencyRegistrator"></param>
+        public BuilderRegistrationService(IDependencyRegistrator dependencyRegistrator)
+        {
+            _dependencyRegistrator = dependencyRegistrator;
+        }
+
         void IBuilderRegistrationService.RegisterBuilder<TService>(IBuilder<TService> builder)
         {
-            RegisterBuilderProduct(builder);
+            RegistrationHelper.RegisterBuilderProduct(_dependencyRegistrator, builder);
         }
     }   
 }
